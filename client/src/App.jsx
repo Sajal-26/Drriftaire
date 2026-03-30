0fbbe13ec49c0d5cbcdcd8720dd34579fb2325ec
nikunjgadaiya <ngniku13@gmail.com>
2026-03-31 02:38:10 +0530

    Created navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BookingPage from "./pages/BookingPage";
 import AdminDashboard from "./pages/AdminDashboard";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<BookingPage />} />
         <Route path="/admin" element={<AdminDashboard />} />
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../styles/Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      {/* Left Links */}
+      <ul className={styles.leftLinks}>
+        <li><Link to="/" className={styles.navItem}>Home</Link></li>
+        <li><Link to="/services" className={styles.navItem}>Services</Link></li>
+        <li><Link to="/booking" className={styles.navItem}>Booking</Link></li>
+      </ul>
+
+      {/* Center Logo */}
+      <Link to="/" className={styles.logoContainer}>
+        <span className={styles.logoText}>Drriftaire</span>
+      </Link>
+
+      {/* Right Links */}
+      <ul className={styles.rightLinks}>
+        <li><Link to="/about" className={styles.navItem}>Why us</Link></li>
+        <li><Link to="/about" className={styles.navItem}>About</Link></li>
+        <li><Link to="/contact" className={styles.navItem}>Contact us</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
