cd67cc3b607574581af3838cd9b146ae0332cb0c
Sajal-26 <schitlangia26@gmail.com>
2026-04-01 20:06:53 +0530

    Added endpoints

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "https://drriftaire2.vercel.app/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://drriftaire.vercel.app/api";
 
 export default API_BASE_URL;
